f4e46cc683b377ad2886cff55bb4cc51ca4faf3a
vinushan20 <vinushan1903@gmail.com>
2025-06-27 21:39:08 +0530

    message

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -1,3 +1,7 @@
-def call() {
-    cleanWs()
+def call(String gitUrl, String gitBranch) {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: gitBranch]],
+        userRemoteConfigs: [[url: gitUrl]]
+    ])
 }
